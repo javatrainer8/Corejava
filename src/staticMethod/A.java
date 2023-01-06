ad17b7698746ea76c2906902b64df2c9be39e213
lucky <javatrainer8@gmail.com>
2023-01-06 15:38:15 +0530

    second

---
+++ src/staticMethod/A.java
@@ -0,0 +1,12 @@
+package staticMethod;
+
+public class A {
+	public static void me() {
+		System.out.println("this is static method example ");
+	}
+
+	public static void main(String[] args) {
+          me();
+	}
+
+}
